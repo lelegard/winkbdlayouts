69bd13ff37030b34968eee5d13c25adcc489221c
Thierry Lelegard <thierry@lelegard.fr>
2023-04-21 17:06:38 +0200

    Fixed install script

--- install.ps1
+++ install.ps1
@@ -3,20 +3,17 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
 param([switch]$NoPause = $false)
 
-# Find architecture directory.
 $OSArch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 $Arch = if ($OSArch -like "*arm*") {"arm64"} elseif ($OSArch -like "*64*") {"x64"} else {"x86"}
+$EndOpt = if ($NoPause) {"-np"} else {"-p"}
 
 if (Test-Path "$PSScriptRoot\$Arch\Release\kbdadmin.exe") {
-    & "$PSScriptRoot\$Arch\Release\kbdadmin.exe" -i "$PSScriptRoot\$Arch\Release"
+    & "$PSScriptRoot\$Arch\Release\kbdadmin.exe" -i "$PSScriptRoot\$Arch\Release" $EndOpt
 }
 elseif (Test-Path "$PSScriptRoot\$Arch\setup.exe") {
-    & "$PSScriptRoot\$Arch\setup.exe"
+    & "$PSScriptRoot\$Arch\setup.exe" $EndOpt
 }
 else {
     Write-Output "No installation program found"
-}
-
-if (-not $NoPause) {
-    pause
+    if (-not $NoPause) {pause}
 }
